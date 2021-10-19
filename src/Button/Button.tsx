337d3775bd43b35c99ff2678c8706749a48fdbff
Mateusz Mistecki <mateusz.mistecki@rockwool.com>
2021-10-19 11:17:12 +0200

    Added colors

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,13 +1,35 @@
 ﻿// import React from "react";
 
+// #95a5a6 concrete
+// #e74c3c alizarin
+// #f39c12 orange
+// #2ecc71 emerald
+
 type ButtonProps = {
-  label: string
+  label: string;
+  // bgColoer: string;
+}
+
+// function Button(props: ButtonProps) {
+// function Button({ label }: ButtonProps) {
+//   return (
+//     // <button {props.label} >Hello</button>
+//     <button { label } >Hello</button>
+//   )
+// }
+
+const styles = {
+  color: 'black',
+  margin: 12,
+  paddintBottom: 10 //CamelCase
 }
 
 function Button(props: ButtonProps) {
-  return (
-    <button>Hello</button>
-  )
+    const { label } = props;
+    // <button {props.label} >Hello</button>
+    return (
+      <button style={ styles } >{label}</button>
+    );
 }
 
 export default Button;
