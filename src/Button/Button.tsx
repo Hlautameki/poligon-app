5a10bd462cdc9a05456db4e9958755e5b4065d21
Mateusz Mistecki <mateusz.mistecki@rockwool.com>
2021-10-19 10:28:30 +0200

    Added Button component

---
+++ src/Button/Button.tsx
@@ -0,0 +1,15 @@
+﻿// import React from "react";
+
+type ButtonProps = {
+  label: string
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button>Hello</button>
+  )
+}
+
+export default Button;
+
+// export { Button };
