6927cca765bde60bc972dea185c8d53a3b5b95f8
Mateusz Mistecki <mateusz.mistecki@rockwool.com>
2021-10-19 11:46:18 +0200

    Added two more buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <Button label='Hello!!!' bgColor={BGColors.alizarin}/>
+      <Button label='Hello!!!' bgColor={BGColors.emerald}/>
+      <Button label='Hello!!!' bgColor={BGColors.concrete}/>
     </div>
   );
 }
